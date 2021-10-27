15d7152407d645c18c4ad5144f5edbf4172ff8f2
iurisajaia <sajaiaiuri@gmail.com>
2021-10-28 01:53:17 +0400

    parse and cache

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+                padding : 50px;
+            }
+
+
+            .links > a {
+                color: #636b6f;
+                padding: 10px 25px;
+                margin-bottom: 5px;
+                border: 1px solid black;
+                font-size: 15px;
+                border-radius: 10px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+                display: block
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                
+                <h1>ნეტგაზეთი</h1>
+                <div class="links">
+                    @if($netgazeti)
+                        @foreach($netgazeti as $net)
+                            <a href="{{$net->href}}" target="_blank">{{$net->title}}</a>
+                        @endforeach
+                    @endif
+                </div>
+
+                <h1>ON.GE</h1>
+                <div class="links">
+                    @if($onge)
+                        @foreach($onge as $net)
+                            <a href="{{$net->href}}" target="_blank">{{$net->title}}</a>
+                        @endforeach
+                    @endif
+                </div>
+
+                <h1>რადიო თავისუფლება</h1>
+                <div class="links">
+                    @if($radio)
+                        @foreach($radio as $net)
+                            <a href="https://www.radiotavisupleba.ge{{$net->href}}" target="_blank">{{$net->title}}</a>
+                        @endforeach
+                    @endif
+                </div>
+
+
+                <h1>IPN</h1>
+                <div class="links">
+                    @if($ipn)
+                        @foreach($ipn as $net)
+                            <a href="https://www.interpressnews.ge/ka/{{$net->url}}" target="_blank">{{$net->title}}</a>
+                        @endforeach
+                    @endif
+                </div>
+
+                <h1>1TV</h1>
+                <div class="links">
+                    @if($public)
+                        @foreach($public as $net)
+                            <a href="{{$net->post_permalink}}" target="_blank">{{$net->post_title}}</a>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,81 @@
+<?php
+use Goutte\Client;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Redis;
+
+
+function getNetgazeti(){
+    $netgazeti = Cache::remember("netgazeti" , now()->addMinutes(10), function(){
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://netgazeti.ge/category/news/');
+        return $crawler->filter('div.col-lg-4 > a')->each(function ($node){
+            $object = new stdClass();
+            $object->href = $node->attr('href');
+            $object->title = $node->attr('title');
+            return $object;
+        });
+    });
+    return $netgazeti;
+}
+
+function getRadio(){
+    $radio = Cache::remember("radio" , now()->addMinutes(10), function(){
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://www.radiotavisupleba.ge/');
+        return $crawler->filter('.media-block__content > a')->each(function ($node){
+            $object = new stdClass();
+            $object->href = $node->attr('href');
+            $object->title = $node->text();
+            return $object;
+        });
+    });
+    return $radio;
+}
+
+function getOnGe(){
+    $onge = Cache::remember("onge" , now()->addMinutes(10), function(){
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://on.ge/news');
+        return $crawler->filter('.swiper-wrapper > li > a')->each(function ($node){
+            $object = new stdClass();
+            $object->href = $node->attr('href');
+            $object->title = $node->text();
+            return $object;
+        });
+    });
+    return $onge;
+}
+
+function getIpn(){
+    $ipn = Cache::remember("ipn" , now()->addMinutes(10), function(){
+        $response = Http::post('https://www.interpressnews.ge/ka/api/home/articles/' , [
+            'page' => 1,
+            'loaded' => 0,
+            'csrfmiddlewaretoken' => "yPdsn"
+        ]);
+        $res = json_decode($response->body());
+        return $res->articles;
+    });
+    return $ipn;
+}
+
+function getPublic(){
+    $public = Cache::remember("public" , now()->addMinutes(10), function(){
+        $response = Http::get('https://1tv.ge/wp-json/listing/all/news?offset=0&lang=ge&post_type=news&topic=&tpl_ver=38.8');
+        return json_decode($response->body());
+    });
+    return $public;
+}
+
+Route::get('/', function () {
+    return response()->view('welcome' , [
+        'netgazeti' => getNetgazeti() , 
+        'ipn' => getIpn(),
+        'onge' => getOnGe(),
+        'public' => getPublic(),
+        'radio' => getRadio()
+    ]);
+
+});
